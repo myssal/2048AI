aa562338cac4234488b34a1bc0e087db1fae4535
Myssal <143514975+myssal@users.noreply.github.com>
2025-04-14 00:26:54 +0700

    feat: update tile

--- Assets/Scripts/Tiles/Tile.cs
+++ Assets/Scripts/Tiles/Tile.cs
@@ -1,10 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
     public TileState state { get; private set; }
     public TileCell cell { get; private set; }
     public int number { get; private set; }
+
+    private Image background;
+    private TextMeshProUGUI text;
+
+    private void Awake()
+	{
+		background = GetComponent<Image>();
+		text = GetComponentInChildren<TextMeshProUGUI>();
+	}
+
+	public void SetState(TileState state, int number)
+    {
+        this.state = state;
+        this.number = number;
+
+        background.color = state.backgroundColor;
+		text.color = state.textColor;
+        text.text = number.ToString();
+	}
 }
